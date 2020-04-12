1810f78c17bd7142c478f0cda13cf3619330d557
msuhaib2020@hotmail.com <msuhaib2020@hotma.com>
2020-04-12 21:52:16 +0530

    job card design

---
+++ ERP_POC/Controllers/JobCardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP_POC.Controllers
+{
+    public class JobCardController : Controller
+    {
+        // GET: JobCard
+        public ActionResult JobCard()
+        {
+            return View();
+        }
+    }
+}
